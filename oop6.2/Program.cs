68bf62670a503da5d432ab1d634f7c682c6a2a5c
Aleksandr <Tet3.teterin@yandex.ru>
2020-06-20 09:08:28 +0500

    Добавьте файлы проекта.

---
+++ oop6.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class classone = new Class();
+            classone.Display();
+            Class classtwo = new Class("12345578910");
+            classtwo.Display();
+            Class classthre = new Class("123");
+            classthre.Display();
+            Console.ReadKey();
+        }
+    }
+}
+class Class
+{
+    private string Metod;
+
+    public Class()
+    {
+        Metod = "Пусcто";
+    }
+    public Class(string Line)
+    {
+        if (Line.Length > 10)
+            this.Metod = "Слишком длинная строка";
+        else
+            this.Metod = Line;
+    }
+    public void Display()
+    {
+        Console.WriteLine(Metod);
+    }
+}
